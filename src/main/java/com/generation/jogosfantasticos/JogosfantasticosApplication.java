3a56995f2c94aa9a6dcd75750ae3fb4c8f18602b
Henrique M. N. <henriquemancininevado@gmail.com>
2023-12-14 11:03:35 -0300

    Criação do projeto Spring

---
+++ src/main/java/com/generation/jogosfantasticos/JogosfantasticosApplication.java
@@ -0,0 +1,13 @@
+package com.generation.jogosfantasticos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JogosfantasticosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JogosfantasticosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generation/jogosfantasticos/JogosfantasticosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.jogosfantasticos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JogosfantasticosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
